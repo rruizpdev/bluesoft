30407c15777d886b9a25ee1ed4bc93b259f482cb
Roberto Carlos Ruiz Poveda <rpoveda@truelogic.io>
2024-04-21 15:18:01 -0500

    Creating Branch repository.

---
+++ Bluesoft.Bank/Bluesoft.Bank.Data/Repositories/BranchRepository.cs
@@ -0,0 +1,19 @@
+﻿using Bluesoft.Bank.Data.Contexts;
+using Bluesoft.Bank.Data.Entities;
+using RCRP.Common.DataAccess.Repositories;
+using System.Linq.Expressions;
+
+namespace Bluesoft.Bank.Data.Repositories;
+
+public class BranchRepository
+    : RepositoryBase<Account>
+{
+    public BranchRepository(BluesoftBankContext dbContext)
+        : base(dbContext, dbContext.Set<Account>())
+    {
+    }
+
+    protected override Expression<Func<Account, bool>> FilterByKeyPredicate(
+        params object[] keyValues)
+        => t => t.Id == (int)keyValues[0];
+}
