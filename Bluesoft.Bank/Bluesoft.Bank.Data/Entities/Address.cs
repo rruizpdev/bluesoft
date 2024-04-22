ead3c5d1b15a7e3aa9d8c5b07429ab8643a030d8
Roberto Carlos Ruiz Poveda <rpoveda@truelogic.io>
2024-04-22 08:59:32 -0500

    Fxing project file and cleaning code.

--- Bluesoft.Bank/Bluesoft.Bank.Data/Entities/Address.cs
+++ Bluesoft.Bank/Bluesoft.Bank.Data/Entities/Address.cs
@@ -1,11 +1,21 @@
 ﻿namespace Bluesoft.Bank.Data.Entities;
 
 #nullable disable
+
+/// <summary>
+/// Estandarizes the addres in the whole system.
+/// </summary>
 public class Address
 {
     public int Id { get; set; }
     public string Street1 { get; set; } = string.Empty;
+
+    //Debería tener una lista de Estados/Departamentos/Provincias
+    //pero eso haría más largo el problema y la prueba ya es bastante larga.
     public string State { get; set; } = string.Empty;
+
+    //Debería tener una lista de Ciudades
+    //pero eso haría más largo el problema y la prueba ya es bastante larga.
     public string City { get; set; } = string.Empty;
     public string ZipCode { get; set; } = string.Empty;
 }
